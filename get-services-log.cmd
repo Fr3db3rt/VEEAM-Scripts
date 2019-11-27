a17fdd4d63ebe46044f1b867338b69ba40d18f2f
Fr3db3rt <fr3db3rt@gmail.com>
2019-11-27 15:19:54 +0100

    Update get-services-log.cmd

--- get-services-log.cmd
+++ get-services-log.cmd
@@ -1,1 +1,1 @@
-PowerShell -ExecutionPolicy Bypass -Command "& {(get-service | tee-object -filepath C:\fastviewer\services.txt)}"
+PowerShell -ExecutionPolicy Bypass -Command "& {(get-service | tee-object -filepath .\services.txt)}"
